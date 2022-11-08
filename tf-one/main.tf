44aa39309749961f2ff542e499ca22670b42a844
Russ Parmer <russell.parmer@weave.works>
2022-11-08 06:20:54 -0700

    initial commit

---
+++ tf-one/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "demo" {
+  name = "demo_sg"
+}
+
+output "sg" {
+  value = aws_security_group.demo
+}
+
+output "sg_id" {
+  value = aws_security_group.demo.id
+}
